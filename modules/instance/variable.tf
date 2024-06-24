8a23ba9c97101621b0249e8fec8fd8f3561c5ad5
Bhushan-88 <bhushandurgawli1@gmail.com>
2024-06-24 17:53:33 +0530

    --- Auto Git Commit ---

--- modules/instance/variable.tf
+++ modules/instance/variable.tf
@@ -1,8 +1,8 @@
 variable "count" {}
-varibale "image_id" {}
+variable "image_id" {}
 variable "instance_type" {}
 variable "key_pair" {}
-varibale "project" {}
+variable "project" {}
 variable "env" {}
 variable "subnet_id" {}
 variable "sg_ids" {
