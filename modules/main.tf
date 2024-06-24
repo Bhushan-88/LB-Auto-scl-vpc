97597875703112e583e646cd8b78ec37a49968e9
Bhushan-88 <bhushandurgawli1@gmail.com>
2024-06-24 17:50:12 +0530

    --- Auto Git Commit ---

---
+++ modules/main.tf
@@ -0,0 +1,17 @@
+resource "aws_vpc" "my_vpc" {
+    cidr_block = "10.10.0.0/16"
+    tags = {
+      Name = "${var.project}-vpc"
+      env = var.env
+    }
+}
+
+resource "aws_subnet" "pri_subnet" {
+    vpc_id = aws_vpc.my_vpc.id
+    cidr_block = "10.0.1.0/24"
+
+    tags = {
+      Name = "${var.project}"
+      env = var.env
+    }
+}
